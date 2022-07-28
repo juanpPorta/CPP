94b4538cbe36b6e241e4250f2234494e607a9179
Juan <j.porta.gar@gmail.com>
2022-07-28 20:44:21 +0200

    ex00

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,12 +1,72 @@
 #include <string>
-#include "Conversion.hpp"
+#include <math.h>
+#include <iostream>
 
-int main()
+
+void toChar(double numWord)
 {
+	char charo;
+	charo = static_cast<char>(numWord);
+	if (charo >= 32 && charo <= 126)
+		std::cout<<"char: "<<charo<<std::endl;
+	else if (charo != numWord)
+		std::cout<<"char: Impossible"<<std::endl;
+	else
+		std::cout<<"char: No Displayable"<<std::endl;
+}
 
-	std::string Frase = "H";
-	Conversion *pepe1 = new Conversion(Frase);
-	
-	delete pepe1;
+void toInt(double numWord)
+{
+	int number;
+
+	number = static_cast<int>(numWord);
+	if(number != numWord)
+		std::cout<<"int: Imposible"<<std::endl;
+	else
+		std::cout<<"int: "<<number<<std::endl;
+}
+
+void toFloat(double numWord)
+{
+	float number;
+	number = static_cast<float>(numWord);
+	if (roundf(number) != numWord || number < -999999 || number > 999999)
+		std::cout<<"float: "<<number<<"f"<<std::endl;
+	else 
+		std::cout<<"float: "<<number<<".0f"<<std::endl;
+}
+
+void todouble(double numWord)
+{
+	if (numWord != numWord || numWord < -999999 || numWord > 999999)
+		std::cout<<"double: "<<numWord<<std::endl;
+	else
+		std::cout<<"double: "<<numWord<<".0"<<std::endl;
+
+}
+
+int main(int argc, char**argv)
+{
+	std::string Num;
+	double numWord;
+
+
+	if (argc != 2)
+	{
+		std::cout<<"numero de argumentos no valido"<<std::endl;
+		return(0);
+	}
+	Num = argv[1];
+	try{
+
+		numWord = std::stod(Num);
+
+		toChar(numWord);
+		toInt(numWord);
+		toFloat(numWord);
+		todouble(numWord);
+	}catch(const std::exception &e){
+		std::cerr<<e.what()<<std::endl;
+	}
 	return(0);
 }
